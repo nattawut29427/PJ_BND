f73ac7fa81bf9c71229acad9ce82593c05fe2900
NATTAWUT_IN <150765583+nattawut29427@users.noreply.github.com>
2025-02-27 22:43:11 +0700

    fix layout myorder page

--- app/myorder/layout.tsx
+++ app/myorder/layout.tsx
@@ -1,15 +1,13 @@
-"use client";
-import { SessionProvider } from "next-auth/react";
+'use client';
+import { SessionProvider } from "next-auth/react"; 
 import { ReactNode } from "react";
-import Drawer from "@/app/admin/components/Drawer";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <SessionProvider>
-      <div className="absolute top-5 left-5">
-        <Drawer />
+      <div>
+        {children}
       </div>
-      <div className="px-72 mx-auto">{children}</div>
     </SessionProvider>
   );
 }
