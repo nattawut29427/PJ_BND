a5ffa5476666cce8b64037cf751a8a137675bed9
Net1phong <netiphong.singkaew@gmail.com>
2025-03-01 20:38:49 +0700

    🔨login page edit myorder

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,6 +3,8 @@ import { signIn } from "next-auth/react";
 import { useState, useCallback, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
+import logo from "@/public/logo.png";
 
 const SignIn = () => {
   const [email, setEmail] = useState("");
@@ -51,56 +53,78 @@ const SignIn = () => {
   }, [router]);
 
   return (
-    <div className="max-w-sm mx-auto p-4">
-      <h1 className="text-3xl font-bold text-center mb-8">Sign In</h1>
+    <>
+      <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-red-950 py-10">
+        <div className="flex shadow-md">
+        <div className="flex flex-wrap content-center justify-center rounded-l-md w-96 h-[32rem] bg-white">
+            <Image
+              src={logo} // Use the imported logo variable
+              alt="Logo"
+              className="w-fit h-fit object-cover rounded-r-md"
+            />
+          </div>
+          <div className="flex flex-wrap content-center justify-center rounded-r-md bg-white w-96 h-[32rem]">
+            <div className="w-72">
+              <h1 className="text-3xl font-bold text-center text-red-500 mb-8">
+                Sign In
+              </h1>
 
-      {error && <p className="text-red-500 text-center mb-4">{error}</p>}
+              {error && (
+                <p className="text-red-500 text-center mb-4">{error}</p>
+              )}
 
-      <form onSubmit={handleSubmit} className="space-y-5">
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            placeholder="Email"
-            className="w-full p-2 border rounded-md text-base font-medium"
-          />
+              <form onSubmit={handleSubmit} className="space-y-5">
+                <div>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    placeholder="Email"
+                    className="w-full p-2 border rounded-md text-lg font-medium bg-white text-gray-600"
+                  />
+                </div>
+
+                <div>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    placeholder="Password"
+                    className="w-full p-2 border rounded-md text-lg font-medium bg-white text-gray-600"
+                  />
+                </div>
+
+                <button
+                  type="submit"
+                  disabled={loading}
+                  className="w-full p-2 bg-red-500 text-white text-lg font-semibold rounded-md"
+                >
+                  {loading ? "Signing In..." : "Sign In"}
+                </button>
+              </form>
+
+              <div className="flex divider text-sm font-semibold justify-center items-center mt-2 text-gray-400">
+                or
+              </div>
+
+              <div className="mt-2 text-center border rounded-md border-gray-400">
+                <button
+                  onClick={handleGoogleSignIn}
+                  disabled={loading}
+                  className="w-full p-2 bg-white text-gray-700 font-semibold rounded-md text-lg "
+                >
+                  <i className="fa-brands fa-google mr-3"></i>
+                  {loading ? "Google..." : "Google"}
+                </button>
+              </div>
+            </div>
+          </div>
+
         </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            placeholder="Password"
-            className="w-full p-2 border rounded-md text-base font-medium"
-          />
-        </div>
-        <button
-          type="submit"
-          disabled={loading}
-          className="w-full p-2 bg-blue-500 text-white font-semibold rounded-md"
-        >
-          {loading ? "Signing In..." : "Sign In"}
-        </button>
-      </form>
-
-      <div className="flex divider text-sm font-bold justify-center items-center mt-2 text-gray-400">
-        or
       </div>
-
-      <div className="mt-2 text-center">
-        <button
-          onClick={handleGoogleSignIn}
-          disabled={loading}
-          className="w-full p-2 bg-gray-500 text-white rounded-md"
-        >
-          <i className="fa-brands fa-google mr-3"></i>
-          {loading ? "Google..." : "Google"}
-        </button>
-      </div>
-    </div>
+    </>
   );
 };
 
