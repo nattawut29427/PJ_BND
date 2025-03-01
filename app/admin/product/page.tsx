63aa0e96ab2f5f65f101a7327a017784eec70f4f
NATTAWUT_IN <150765583+nattawut29427@users.noreply.github.com>
2025-03-02 04:19:12 +0700

    🚀V.8

--- app/admin/product/page.tsx
+++ app/admin/product/page.tsx
@@ -21,7 +21,7 @@ import {
   Spinner,
   Image,
 } from "@heroui/react";
-import React, { useEffect, useState, SVGProps } from "react";
+import React, { useEffect, useState } from "react";
 
 import ModalPD from "@/app/admin/product/Component/ModalPD";
 import Find from "@/components/Find";
@@ -29,11 +29,9 @@ import ModalEdit from "@/app/admin/product/Component/ModalEdit";
 import Deleat from "@/app/admin/product/Component/Deleat";
 
 
-export type IconSvgProps = SVGProps<SVGSVGElement> & {
-  size?: number;
-};
 
-export const columns = [
+
+const columns = [
   { name: "No", uid: "No" },
   { name: "Id", uid: "id" },
   { name: "Images", uid: "images" },
@@ -44,33 +42,33 @@ export const columns = [
   { name: "DELEAT", uid: "deleat" },
 ];
 
-export const EyeIcon = (props: IconSvgProps) => (
-  <svg
-    aria-hidden="true"
-    fill="none"
-    focusable="false"
-    height="1em"
-    role="presentation"
-    viewBox="0 0 20 20"
-    width="1em"
-    {...props}
-  >
-    <path
-      d="M12.9833 10C12.9833 11.65 11.65 12.9833 10 12.9833C8.35 12.9833 7.01666 11.65 7.01666 10C7.01666 8.35 8.35 7.01666 10 7.01666C11.65 7.01666 12.9833 8.35 12.9833 10Z"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={1.5}
-    />
-    <path
-      d="M9.99999 16.8916C12.9417 16.8916 15.6833 15.1583 17.5917 12.1583C18.3417 10.9833 18.3417 9.00831 17.5917 7.83331C15.6833 4.83331 12.9417 3.09998 9.99999 3.09998C7.05833 3.09998 4.31666 4.83331 2.40833 7.83331C1.65833 9.00831 1.65833 10.9833 2.40833 12.1583C4.31666 15.1583 7.05833 16.8916 9.99999 16.8916Z"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth={1.5}
-    />
-  </svg>
-);
+// export const EyeIcon = (props: IconSvgProps) => (
+//   <svg
+//     aria-hidden="true"
+//     fill="none"
+//     focusable="false"
+//     height="1em"
+//     role="presentation"
+//     viewBox="0 0 20 20"
+//     width="1em"
+//     {...props}
+//   >
+//     <path
+//       d="M12.9833 10C12.9833 11.65 11.65 12.9833 10 12.9833C8.35 12.9833 7.01666 11.65 7.01666 10C7.01666 8.35 8.35 7.01666 10 7.01666C11.65 7.01666 12.9833 8.35 12.9833 10Z"
+//       stroke="currentColor"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       strokeWidth={1.5}
+//     />
+//     <path
+//       d="M9.99999 16.8916C12.9417 16.8916 15.6833 15.1583 17.5917 12.1583C18.3417 10.9833 18.3417 9.00831 17.5917 7.83331C15.6833 4.83331 12.9417 3.09998 9.99999 3.09998C7.05833 3.09998 4.31666 4.83331 2.40833 7.83331C1.65833 9.00831 1.65833 10.9833 2.40833 12.1583C4.31666 15.1583 7.05833 16.8916 9.99999 16.8916Z"
+//       stroke="currentColor"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       strokeWidth={1.5}
+//     />
+//   </svg>
+// );
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const statusColorMap: Record<string, ChipProps["color"]> = {
