9a6d5cf37804746af23e350e7aa0064e4ff85e1d
NATTAWUT_IN <150765583+nattawut29427@users.noreply.github.com>
2025-03-02 07:12:28 +0700

    V.20

--- app/api/Category/route.ts
+++ app/api/Category/route.ts
@@ -1,35 +1,35 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET() {
-  try {
-    const category = await prisma.category.findMany();
+// export async function GET() {
+//   try {
+//     const category = await prisma.category.findMany();
    
-    return new Response(JSON.stringify(category), { status: 200 });
+//     return new Response(JSON.stringify(category), { status: 200 });
  
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Unable to fetch skewer' }), { status: 500 });
-  }
-}
+    
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: 'Unable to fetch skewer' }), { status: 500 });
+//   }
+// }
 
-export async function POST(req: any) {
-  try {
-    const body = await req.json();
-    const { name } = body;
+// export async function POST(req: any) {
+//   try {
+//     const body = await req.json();
+//     const { name } = body;
 
-    if (!name) {
-      return new Response(JSON.stringify({ error: 'Name' }), { status: 400 });
-    }
+//     if (!name) {
+//       return new Response(JSON.stringify({ error: 'Name' }), { status: 400 });
+//     }
 
-    const newCategory = await prisma.category.create({
-      data: { name },
-    });
+//     const newCategory = await prisma.category.create({
+//       data: { name },
+//     });
 
-    return new Response(JSON.stringify(newCategory), { status: 201 });
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Unable to create Category' }), { status: 500 });
-  }
-}
+//     return new Response(JSON.stringify(newCategory), { status: 201 });
+//     // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error: 'Unable to create Category' }), { status: 500 });
+//   }
+// }
